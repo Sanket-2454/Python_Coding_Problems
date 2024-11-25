78e03af4ffe4a5899cf8869ff24df23769cfe15a
Sanket-2454 <90637167+Sanket-2454@users.noreply.github.com>
2024-11-26 00:18:48 +0530

    Create copy_list.py

---
+++ copy_list.py
@@ -0,0 +1,19 @@
+# apporch 1
+mylist1= [4,8,2,6,9]
+mylist_copy1= mylist[:]
+print(mylist_copy)
+
+# apporch 2
+mylist2= [1,2,3,4,5]
+mylist_copy2= []
+mylist_copy.extend(mylist)
+
+#apporch 3
+mylist = [8,3,6,5,"hello"]
+new = mylist.copy()
+print(new)
+
+# apporch 4
+mylist4 = [60,30,40]
+mylist4_copy = list(mylist4)
+print(mylist4_copy)
