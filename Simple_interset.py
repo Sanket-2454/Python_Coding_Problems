000e88bfed814838ee072cb048a797b843fbf7cd
Sanket-2454 <90637167+Sanket-2454@users.noreply.github.com>
2024-11-26 00:07:18 +0530

    Create Simple_interset.py

---
+++ Simple_interset.py
@@ -0,0 +1,6 @@
+def Simple_Interset():
+  amount = int(input("Enter the amount:"))
+  rate = int(input("Enter the rate:"))
+  tenure = int(input("Enter the tenure"))
+  return (amount*rate*tenure)/10
+Simple_Interset()
