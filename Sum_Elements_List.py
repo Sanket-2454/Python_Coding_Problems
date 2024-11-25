7bafc66a02061165121615cfaf2eb2c9aa4699a5
Sanket-2454 <90637167+Sanket-2454@users.noreply.github.com>
2024-11-26 00:43:43 +0530

    Create Sum_Elements_List.py

---
+++ Sum_Elements_List.py
@@ -0,0 +1,11 @@
+def calculate_sum(num):
+    total = 0  # intalize the toatl 
+    for i in num:
+        total = total + i
+    return total
+
+data = [10, 20, 30, 40]
+total_sum = calculate_sum(data)
+print(total_sum)
+
+  
